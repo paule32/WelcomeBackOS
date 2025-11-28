628264224ca67c8dbb9bf3700556be1d37716a22
paule32 <paule32.jk@gmail.com>
2025-11-28 13:19:26 +0100

    update: compressed data do not create depacked file

--- src/loader.asm
+++ src/loader.asm
@@ -3,13 +3,50 @@
 ; \note  (c) 2025 by Jens Kallup - paule32
 ;        all rights reserved.
 ; ---------------------------------------------------------------------------
-; loader.asm – Minimaler 16-Bit-DOS-Loader, der KERNEL.BIN lädt
+; loader.asm – minimal 16-Bit-DOS-Loader, that load KERNEL.BIN.
 ; Build: nasm -f bin loader.asm -o LOADER.COM
-
+; ---------------------------------------------------------------------------
 org  0x100               ; .COM start offset
 bits 16
 
 start:
+    mov  word [ExecBlk + 0x00], 0
+    mov  word [ExecBlk + 0x02], GzipCmdTail  ; OFFSET
+    mov  word [ExecBlk + 0x04], ds           ; SEGMENT
+    mov  word [ExecBlk + 0x06], 0x005C       ; FCB1 OFFSET
+    mov  word [ExecBlk + 0x08], cs           ; FCB1 SEGMENT
+    mov  word [ExecBlk + 0x0A], 0x006C       ; FCB2 OFFSET
+    mov  word [ExecBlk + 0x0C], cs           ; FCB2 SEGMENT
+
+    ; -------------------------------------------------------
+    ; EXEC AH=4Bh, AL=00h (Load & Execute) ...
+    ; -------------------------------------------------------
+    mov  dx, GzipAppName    ; ASCIIZ "gzip.exe"
+    push ds
+    pop  es
+    mov  bx, ExecBlk        ; Parameterblock
+    mov  ax, 0x4B00
+    int  0x21
+    jc   go_error           ; CF=1 => Fehler, AX=Errorcode
+
+    ; -------------------------------------------------------
+    ; optional: return code of child (INT 21h/4Dh)
+    ; -------------------------------------------------------
+    mov  ax, 0x4D00
+    int  0x21
+    jc   go_error
+    jnc  go_forward
+    
+    go_error:
+    mov ax, 0x4C00
+    mov bx, 1
+    int 0x21
+    
+    go_forward:
+    mov ax, 0x4C00
+    mov bx, 0
+    int 0x21
+
     ; initialize segments
     cli
     mov ax, cs
@@ -75,8 +112,9 @@ done_read:
     push edx
     jmp far dword [esp]
 
-; ------------------ Erorr Handling ------------------
-
+; ---------------------------------------------------------------------------
+; Erorr Handling
+; ---------------------------------------------------------------------------
 file_error:
     mov dx, msg_file
     mov ah, 0x09
@@ -93,8 +131,9 @@ exit_dos:
     mov ax, 0x4C01       ; DOS: terminate with error code 1
     int 0x21
 
-; ---------------------- GDT ---------------------------
-
+; ---------------------------------------------------------------------------
+; GDT ...
+; ---------------------------------------------------------------------------
 align 8
 gdt_start:
     dq 0x0000000000000000       ; Null-Deskriptor
@@ -106,7 +145,9 @@ gdt_descriptor:
     dw gdt_end - gdt_start - 1
     dd gdt_start
 
-; ----------------- 32-Bit Code ------------------------
+; ---------------------------------------------------------------------------
+; 32-Bit Code ...
+; ---------------------------------------------------------------------------
 bits 32
 protected_mode_entry:
     ; Segmente setzen (Selector 0x10 = Data-Segment)
@@ -126,9 +167,25 @@ protected_mode_entry:
 hang:
     jmp hang
 
-; ------------------ Daten (16-Bit) --------------------
+; ---------------------------------------------------------------------------
+; Data (16-Bit)
+; ---------------------------------------------------------------------------
 bits 16
 kernel_fname db "KERNEL.BIN",0
 
 msg_file db "open error: KERNEL.BIN$",0
 msg_read db "read error: KERNEL.BIN$",0
+
+GzipAppName:    db 'GZ.EXE', 0
+GzipErrorMsg:   db 'exec of gzip.exe failed.', 0
+
+GzipCmdTail:
+    db GzipCmdTextEnd - GzipCmdText          ; Länge
+GzipCmdText:
+    db ' -d KERNEL.BIZ'
+GzipCmdTextEnd:
+    db 13
+
+section .bss
+; EXEC-Parameterblock (14 Bytes)
+ExecBlk:        resb 14
